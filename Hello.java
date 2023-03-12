70d29fdce538d7f1aa604fb430ebf8feb41640d3
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-12 14:20:54 +0000

    文字の連結

--- Hello.java
+++ Hello.java
@@ -1,6 +1,13 @@
-class Hello{
-  public static void main(String[] args){
-    System.out.println(17);
-    System.out.println(5 + 3);
+class Hello {
+  public static void main(String[] args) {
+    // "こんにちは"と"世界"を連結して出力してください
+    System.out.println("こんにちは" + "世界");
+    
+    // "38"と"19"を連結して出力してください
+    System.out.println("38" + "19");
+    
+    // 38と19を足して出力してください
+    System.out.println(38 + 19);
+    
   }
 }
