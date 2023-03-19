aee441a7d05afd734a5f1d517f71db73f7375b07
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-19 09:36:26 +0000

    for文

--- Hello.java
+++ Hello.java
@@ -1,23 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    int number = 1;
-
-    // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
-    while (number <= 30) {
-      if (number % 15 == 0) {
-        System.out.println("FizzBuzz");
-      }
-      else if (number % 3 == 0) {
-        System.out.println("Fizz");
-      }
-      else if (number % 5 == 0) {
-        System.out.println("Buzz");
-      }
-      else {
-        System.out.println(number);
-      }
-
-      number++;
-    }
+    // for文を用いて、繰り返し処理をつくってください
+    for (int i = 1; i <= 10; i++)
+    System.out.println(i + "回目のループです");
   }
 }
