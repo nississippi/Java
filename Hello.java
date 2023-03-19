335f9e40aff5340436fe0dc18770ee964189a0fc
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-19 14:28:05 +0000

    メソッド

--- Hello.java
+++ Hello.java
@@ -1,11 +1,17 @@
 class Main {
   public static void main(String[] args) {
-    String[] names = {"にんじゃわんこ", "ひつじ仙人", "ベイビーわんこ"};
-    
-    // 配列用のfor文を用いて、「私の名前は◯◯です」と出力してください
-    for (String name:names) {
-      System.out.println("私の名前は" + name + "です");
-    }
-    
+    // 引数に「Kate Jones」を渡してください
+    printData("Kate Jones");
+
+    // 引数に「John Christopher Smith」を渡してください
+    printData("John Christopher Smith");
+
+  }
+
+  // 引数を受け取るようにしてください
+  public static void printData(String name) {
+    // 「私の名前は◯◯です」と出力されるように書き換えてください
+    System.out.println("私の名前は" + name + "です");
+
   }
 }
