3d8d6ddb4a272bc99495b0f4ab918087f2e23a66
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-19 09:31:59 +0000

    FizzBuzz

--- Hello.java
+++ Hello.java
@@ -1,11 +1,23 @@
 class Main {
   public static void main(String[] args) {
-    int number = 10;
-    
+    int number = 1;
+
     // while文を用いて、numberが0より大きい場合に繰り返す、繰り返し処理を作ってください
-    while (number > 0) {
-      System.out.println(number);
-      number--;
+    while (number <= 30) {
+      if (number % 15 == 0) {
+        System.out.println("FizzBuzz");
+      }
+      else if (number % 3 == 0) {
+        System.out.println("Fizz");
+      }
+      else if (number % 5 == 0) {
+        System.out.println("Buzz");
+      }
+      else {
+        System.out.println(number);
+      }
+
+      number++;
     }
   }
 }
