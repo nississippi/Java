78e7ef009fcac80c9a0d92987b8447a048462a1f
nississippi <108444184+nississippi@users.noreply.github.com>
2023-03-22 15:17:54 +0000

    カプセル化失敗

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ class Main {
   }
 
   // 引数を受け取るようにしてください
-  public static void printData(String name) {
+  private static void printData(String name) {
     // 「私の名前は◯◯です」と出力されるように書き換えてください
     System.out.println("私の名前は" + name + "です");
 
